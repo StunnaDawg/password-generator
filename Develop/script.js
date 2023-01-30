1060a4fd5eb2213aa460fbc3b9a14581a196bbb9
StunnaDawg <jonsonallen9@gmail.com>
2023-01-30 18:08:00 -0400

    alert function

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,19 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); //assigns generateBtn on to the event listener so it is clickable
+
+function generatePassword() {
+  prompt('hi')
+  prompt('hi again')
+  prompt('hi x3')
+} 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); //generate password function will create random password depending on the users choice.
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.value = password; //takes the generate() function and passes it on to the query.Selector
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword); //when button is clicked calls write password function.
